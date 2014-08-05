e5db13e37c853786594ad1d39dea9de395205725
Stachu Korick <StachuKorick@gmail.com>
2014-08-05 07:28:17 -0400

    Install-Package XUnit, Install-Package Nancy.Testing, add tests

---
+++ Guesstimate/Tests/HomeModuleTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy.Testing;
+using Web.Modules;
+using Nancy;
+
+namespace Tests
+{
+    [TestClass]
+    public class HomeModuleTests
+    {
+        private Browser sut;
+
+        public HomeModuleTests()
+        {
+            sut = new Browser(new DefaultNancyBootstrapper());
+        }
+
+        [TestMethod]
+        public void TestModuleHelloWorldShouldReturnHelloImNancy()
+        {
+            var actual = sut.Get("/helloworld/");
+
+            Assert.AreEqual("Hello, I'm Nancy!", actual.Body.AsString());
+        }
+
+        [TestMethod]
+        public void TestModuleHelloOtherShouldReturnHelloImNancy()
+        {
+            var suffix = "test";
+
+            var actual = sut.Get("/hello/" + suffix);
+
+            Assert.AreEqual("Hello, " + suffix, actual.Body.AsString());
+        }
+    }
+}
