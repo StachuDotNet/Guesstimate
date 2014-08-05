65f95dee1b38cfdae3a659261a75dbe4024a82e4
Stachu Korick <StachuKorick@gmail.com>
2014-08-05 00:27:36 -0400

    Example SignalR hub

---
+++ Guesstimate/Web/Hubs/TestHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Hubs
+{
+    public class TestHub : Hub
+    {
+        public void ServerFunc(string bar)
+        {
+            Clients.All.ClientFunc("Hello!");
+        }
+    }
+}
