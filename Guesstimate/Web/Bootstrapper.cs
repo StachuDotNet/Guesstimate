2a0ed0fae99f592ba5b6a5aeaddf71cd56c66355
Stachu Korick <StachuKorick@gmail.com>
2014-08-04 23:12:00 -0400

    Adding bootstrapper to deal with static content in non-/Content folder

---
+++ Guesstimate/Web/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Nancy;
+using Nancy.Conventions;
+using Nancy.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("/assets", "Assets")
+            );
+        }
+    }
+}
