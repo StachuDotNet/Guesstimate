0eccd8bfc3cf7ce5cea8718e11768872921bbfbb
Stachu Korick <StachuKorick@gmail.com>
2014-08-05 06:36:18 -0400

    Adding IRepository

---
+++ Guesstimate/Web/Persistance/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.Models;
+
+namespace Web.Persistance
+{
+    public interface IRepository
+    {
+        bool LogOn(string name, string pass);
+
+        bool LogOff(string name, string pass);
+
+        IEnumerable<User> UserList();
+
+        Dictionary<string, string> VoteList();
+
+        bool Vote(string name, string pass, string vote);
+
+        // admin stuff
+        bool ClearVotes(string adminPass);
+
+        bool ClaimAdmin(string adminPass);
+    }
+}
