d714b37c8e3c7b5a3182e490d78bc2c1e8692e13
Stachu Korick <StachuKorick@gmail.com>
2014-08-05 06:34:14 -0400

    Adding models

---
+++ Guesstimate/Web/Models/Vote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class Vote
+    {
+        public int ID { get; set; }
+        public virtual User TeamUser { get; set; }
+        public int PointsApplied { get; set; }
+    }
+}
